2d64ab083e715d325b69618c0091ef87cf2f55e4
Roberto Guevara <robertoguevr7@gmail.com>
2024-01-08 15:28:23 -0600

    Listar tareas

--- DIa 8 - SistemaTareas/src/main/java/gm/tareas/controlador/IndexControlador.java
+++ DIa 8 - SistemaTareas/src/main/java/gm/tareas/controlador/IndexControlador.java
@@ -2,8 +2,12 @@ package gm.tareas.controlador;
 
 import gm.tareas.modelo.Tarea;
 import gm.tareas.servicio.TareaServicio;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,22 +24,37 @@ public class IndexControlador implements Initializable {
 
     @Autowired
     private TareaServicio tareaServicio;
-
     @FXML
     private TableView<Tarea> tareaTabla;
-
     @FXML
     private TableColumn<Tarea, Integer> idTareaColumna;
-
     @FXML
-    private TableColumn<Tarea, String> nombreTareaColumna;
+    private TableColumn<Tarea, String> tareaColumna;
     @FXML
     private TableColumn<Tarea, String> responsableColumna;
     @FXML
     private TableColumn<Tarea, String> estatusColumna;
 
+    private final ObservableList<Tarea> tareaLista = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        tareaTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        configurarColumnas();
+        listarTareas();
+    }
 
+    private void configurarColumnas(){
+        idTareaColumna.setCellValueFactory(new PropertyValueFactory<>("idTarea"));
+        tareaColumna.setCellValueFactory(new PropertyValueFactory<>("nombreTarea"));
+        responsableColumna.setCellValueFactory(new PropertyValueFactory<>("responsable"));
+        estatusColumna.setCellValueFactory(new PropertyValueFactory<>("estatus"));
+    }
+
+    private void listarTareas(){
+        logger.info("Ejecutando listado de tareas");
+        tareaLista.clear();
+        tareaLista.addAll(tareaServicio.listarTareas());
+        tareaTabla.setItems(tareaLista);
     }
 }
