6be5b0c8789cd1f76fe43e658f604634ca9f0f1f
Roberto Guevara <robertoguevr7@gmail.com>
2024-01-11 11:38:33 -0600

    Inicio proyecto JSP Spring Boot

--- 9- Sistema_empleados/src/test/java/gm/empleados/EmpleadosApplicationTests.java
+++ 9- Sistema_empleados/src/test/java/gm/empleados/EmpleadosApplicationTests.java
@@ -1,10 +1,10 @@
-package gm.tareas;
+package gm.empleados;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TareasApplicationTests {
+class EmpleadosApplicationTests {
 
 	@Test
 	void contextLoads() {
--- DIa 8 - SistemaTareas/src/main/java/gm/tareas/repositorio/TareaRepositorio.java
+++
@@ -1,7 +0,0 @@
-package gm.tareas.repositorio;
-
-import gm.tareas.modelo.Tarea;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TareaRepositorio extends JpaRepository<Tarea, Integer> {
-}
