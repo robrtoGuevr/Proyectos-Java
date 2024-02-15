b2868d451dbc19c4a82176f088470271a6d8ee0f
Roberto Guevara <robertoguevr7@gmail.com>
2024-02-15 17:25:54 -0600

    sistemna de contactos completo CRUD

--- 10- Sistema de contactos/src/main/java/gm/contactos/controlador/ContactoControlador.java
+++ 10- Sistema de contactos/src/main/java/gm/contactos/controlador/ContactoControlador.java
@@ -6,8 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +31,38 @@ public class ContactoControlador {
     @GetMapping("/agregar")
     public String mostrarAgregar(){
         return "agregar";
+    }
+
+    @PostMapping("/agregar")
+    public String Agregar(@ModelAttribute("contactoForma") Contacto contacto){
+        logger.info("Contacto a agregar: "+contacto);
+        contactoServicio.guardarContacto(contacto);
+        return("redirect:/");
+    }
+
+
+    @GetMapping("/editar/{id}")
+    public String mostrarEditar(@PathVariable(value = "id") int idContacto, ModelMap modelo){
+        Contacto contacto = contactoServicio.buscarContactoPorId(idContacto);
+        logger.info("Contacto a editar: " +contacto);
+        modelo.put("contacto",contacto);
+        return("editar");
+    }
+
+    @PostMapping("/editar")
+    public String editar(@ModelAttribute("contacto")Contacto contacto){
+        logger.info("Contacto a guardar: "+contacto);
+        contactoServicio.guardarContacto(contacto);
+        return ("redirect:/");
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable(value = "id") int idContacto){
+        Contacto contacto = new Contacto();
+        contacto.setIdContacto(idContacto);
+        logger.info("Contacto a eliminar: "+contacto);
+        contactoServicio.eliminarContacto(contacto);
+        return ("redirect:/");
     }
     
 }
